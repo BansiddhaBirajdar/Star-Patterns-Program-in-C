011e179d89a265d56b21fc8d69b1806bdf54e0b3
bansibirajdar <bansi.birajdar7@gmail.com>
2021-04-17 13:57:56 +0530

    Day1

---
+++ pattern4.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+void display(int v,int h)
+{
+
+	int r,c;
+	if(h==v)
+	{
+		for(r = 0 ; r < v ; r++)
+		{
+			for ( c = 0; c < h; ++c)
+			{
+				printf(" %d ",c+1);
+			}
+			printf("\n");
+		}
+		
+	}
+	else
+	{
+		printf("\ninvalied input");
+	}
+
+
+}
+
+int main()
+{
+	int h,v,r,c;
+	printf("\nEnter the horizontal:");
+	scanf("%d",&h);//4
+	printf("\nEnter the vertical:");
+	scanf("%d",&v);//4
+	display(v,h);
+	return 0;
+}
---
+++ pattern5.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void display(int v,int h)
+{
+
+	int r,c;
+	char ch;
+	if(h==v)
+	{
+		for(r = 0 ; r < v ; r++)
+		{
+			ch='A';
+			for ( c = 0; c < h; ++c)
+			{
+				printf(" %c ",ch);
+				ch++;
+			}
+			printf("\n");
+		}
+		
+	}
+	else
+	{
+		printf("\ninvalied input");
+	}
+
+
+}
+
+int main()
+{
+	int h,v,r,c;
+	printf("\nEnter the horizontal:");
+	scanf("%d",&h);//4
+	printf("\nEnter the vertical:");
+	scanf("%d",&v);//4
+	display(v,h);
+	return 0;
+}
